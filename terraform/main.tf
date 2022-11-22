266dbc2484f94db3c3773de0b28d5a5f98419fb1
64J0 <vinigaio97@gmail.com>
2022-11-22 13:45:35 -0300

    wip: add gw and route table to vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,6 +45,25 @@ resource "aws_vpc" "wordpress" {
   tags             = var.tags
 }
 
+# 1. Internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.wordpress.id
+  tags   = var.tags
+}
+
+# 2. Route table + rota 0.0.0.0/0 "apontando" para o Internet Gateway
+resource "aws_route_table" "wordpress" {
+  vpc_id = aws_vpc.wordpress.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = var.tags
+}
+
+# 3. Association da subnet com a route table
 resource "aws_subnet" "wordpress" {
   vpc_id            = aws_vpc.wordpress.id
   cidr_block        = "10.0.1.0/24"
