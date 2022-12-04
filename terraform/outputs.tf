04788481f8111e3dd4853b7258e41d5dee806b6c
64J0 <vinigaio97@gmail.com>
2022-12-04 11:48:45 -0300

    chore: add the monitoring public ip as an output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,6 @@ output "rds_port" {
   value = module.rds_wordpress.database_port
 }
 
-# output "ec2_monitoring_public_ip" {
-#   value = module.ec2_monitoring.public_ip
-# }
+output "ec2_monitoring_public_ip" {
+  value = module.ec2_monitoring.public_ip
+}
